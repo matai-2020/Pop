40ca7e52eca73d38f8b49dc33876948dbc06f957
Ariana Mirus <63701400+ariana-mirus20@users.noreply.github.com>
2020-06-19 10:40:53 +1200

    ari test push score component

--- client/components/Score.jsx
+++ client/components/Score.jsx
@@ -1,1 +1,21 @@
 
+import React, { Component } from 'react'
+class Score extends Component {
+  render () {
+    return (
+      <div>
+        <h2>$1,000,000.00</h2>
+        <h3>$500,000.00</h3>
+        <h3>$250,000.00</h3>
+        <h3>$125,000.00</h3>
+        <h3>$60,000.00</h3>
+        <h3>$30,000.00</h3>
+        <h4>$15,000.00</h4>
+        <h4>$10,000.00</h4>
+        <h5>$5,000.00</h5>
+        <h5>$1000.00</h5>
+      </div>
+    )
+  }
+}
+export default Score
